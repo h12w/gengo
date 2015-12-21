af4a4dc42b00fba48e2a30d20f5278ff74b3a975
Hǎiliàng Wáng <w@h12.me>
2015-12-21 10:09:27 +0800

    support extensible abbreviaction list

--- name.go
+++ name.go
@@ -4,18 +4,10 @@ import (
 	"strings"
 )
 
-func GoUpperName(s string) string {
-	s = SnakeToUpperCamel(s)
-	if strings.HasSuffix(s, "Id") {
-		s = strings.TrimSuffix(s, "Id") + "ID"
-	}
-	return s
-}
-
 func SnakeToUpperCamel(s string) string {
 	ss := strings.Split(s, "_")
 	for i := range ss {
-		ss[i] = strings.Title(ss[i])
+		ss[i] = upperOrTitle(ss[i])
 	}
 	return strings.Join(ss, "")
 }
@@ -23,7 +15,20 @@ func SnakeToUpperCamel(s string) string {
 func SnakeToLowerCamel(s string) string {
 	ss := strings.Split(s, "_")
 	for i := 1; i < len(ss); i++ {
-		ss[i] = strings.Title(ss[i])
+		ss[i] = upperOrTitle(ss[i])
 	}
 	return strings.Join(ss, "")
+}
+
+var Abbreviations = map[string]struct{}{
+	"cpi": struct{}{},
+	"gif": struct{}{},
+	"id":  struct{}{},
+}
+
+func upperOrTitle(s string) string {
+	if _, ok := Abbreviations[s]; ok {
+		return strings.ToUpper(s)
+	}
+	return strings.Title(s)
 }
